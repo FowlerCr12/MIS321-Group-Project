cb895bd736d3c20b391cd2ff74859d83fa94adfb
ltadams0311 <ltadams@crimson.ua.edu>
2025-04-17 09:18:37 -0500

    Class types added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32984e5582121155942b62c0b0d2288c53c1ec44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a653d90a99d095bd009b5beb4605e7fb57b72fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
